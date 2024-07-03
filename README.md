7252f5cd2d1a99af2d4bb177bd197cb6282fb468
stephane-klein <contact@stephane-klein.info>
2024-07-03 17:36:03 +0200

    First import

---
+++ README.md
@@ -0,0 +1,11 @@
+# Svelte, CodeMirror conceal POC
+
+My note in french about this POC: https://notes.sklein.xyz/Projets/Projet-8
+
+```sh
+$ mise install
+$ pnpm install
+$ pnpm run dev
+$ firefox http://localhost:5173/
+```
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import "./editor.css";
+    import { onMount } from "svelte";
+    import {basicSetup as CodeBasicSetup} from "codemirror";
+    import {EditorView } from "@codemirror/view";
+    import {Compartment} from "@codemirror/state";
+    import {markdown} from "@codemirror/lang-markdown";
+
+    let languageConf = new Compartment;
+    let codeElement;
+    let _codeEditorView;
+
+    onMount(() => {
+        _codeEditorView = new EditorView({
+            parent: codeElement,
+            doc: "Hello world",
+            extensions: [
+                CodeBasicSetup,
+                languageConf.of(markdown())
+            ]
+        });
+    });
+</script>
+<div bind:this={codeElement} class="editor" style="height: 300px; overflow: scroll"></div>
